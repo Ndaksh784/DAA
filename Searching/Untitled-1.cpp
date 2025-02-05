74c23145067121d8f6d0edcc9ecf990509fc2379
Ndaksh784 <152335040+Ndaksh784@users.noreply.github.com>
2025-02-05 20:25:05 +0530

    Seaching

---
+++ Searching/Untitled-1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+int jumpsearch(vector<int>arr,int key){
+    int n=arr.size();
+    int m=sqrt(n);
+    int s=0;
+    int e=s+m;
+    while(e<n && arr[e-1]<key){
+        s=e;
+        e=e+m;
+    }
+    for (int i = s; i < e && i < n; i++) {  
+        if (arr[i] == key) return i;
+    }
+
+    return -1; 
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array"<<endl;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"Enter the key to be searched"<<endl;
+    cin>>key;
+    int ans=jumpsearch(arr,key);
+    if(ans!=-1){
+        cout<<"Key found"<<ans<<endl;
+    }
+    else{
+        cout<<"Not found"<<endl;
+
+    }
+
+}
